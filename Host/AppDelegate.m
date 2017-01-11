0382c0391cbc63bfd01f44d45c00d3e5cf5495e0
Smith, Donnie <donnie.smith@rakuten.com>
2017-01-11 15:35:56 +0900

    REM-18371 Fix review comments - remove custom ViewController class and just use UIViewController in the storyboard - synthesize the window in the delegate implementation - remove didFinishLaunchingWithOptions method

--- Host/AppDelegate.m
+++ Host/AppDelegate.m
@@ -4,16 +4,6 @@
  */
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
-
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    return YES;
-}
-
+@synthesize window;
 @end
