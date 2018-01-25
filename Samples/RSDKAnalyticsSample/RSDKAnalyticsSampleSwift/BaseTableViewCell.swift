124045a722fd4f797923f9694db12aa137f10a7b
Ta, Viet | Vito | GHRD <viet.ta@rakuten.com>
2018-01-25 16:44:39 +0900

    [REM-25524] Add Swift sample

---
+++ Samples/RSDKAnalyticsSample/RSDKAnalyticsSampleSwift/BaseTableViewCell.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+protocol BaseCellDelegate {
+    func update(_ dict: [String: Any])
+}
+
+@IBDesignable class BaseTableViewCell: UITableViewCell {
+
+    var titleLabel: UILabel = UILabel()
+
+    var delegate: BaseCellDelegate?
+
+    var title:String? {
+        didSet {
+            self.update(title: title)
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setup()
+    }
+
+    func setup() {
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(self.titleLabel)
+
+        let leadingConstraint = NSLayoutConstraint(item: self.titleLabel, attribute: .leading, relatedBy: .equal, toItem: self.contentView, attribute: .leading, multiplier: 1.0, constant: 16.0)
+        let verConstraint = NSLayoutConstraint(item: self.titleLabel, attribute: .centerY, relatedBy: .equal, toItem: self.contentView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+        self.contentView.addConstraints([leadingConstraint, verConstraint])
+    }
+
+    func update(_ value: Any) {
+        self.delegate?.update([self.title!: value])
+    }
+
+    func update(title: String?) {
+        if let title = title {
+            self.titleLabel.text = title
+        }
+    }
+}
---
+++ Samples/RSDKAnalyticsSample/RSDKAnalyticsSampleSwift/TextFieldTableViewCell.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class TextFieldTableViewCell: BaseTableViewCell {
+    @IBOutlet weak var textField: UITextField!
+
+    override func update(title: String?) {
+        super.update(title: title)
+        if title == GlobalConstants.kRATAccountID {
+            let accountId = Bundle.main.infoDictionary?["RATAccountIdentifier"] as! Int
+            self.textField.text = String(accountId)
+        } else if title == GlobalConstants.kRATAppID {
+            let appId = Bundle.main.infoDictionary?["RATAppIdentifier"] as! Int
+            self.textField.text = String(appId)
+        }
+    }
+
+    @IBAction func valueChanged(_ sender: Any) {
+        if let value = self.textField.text {
+            self.update(value)
+        }
+    }
+}
