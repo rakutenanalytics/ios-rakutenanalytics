d4cc4d3eaabe523ca6fa1209a7430f089de22905
Smith, Donnie <donnie.smith@rakuten.com>
2018-04-04 17:45:49 +0900

    SDKCF-68 / SDKCF-22 Add support for multiple targets using different module subspecs
    - add runtime check for RATTracker class availability to manager initSharedInstance
    - add Core subspec tests

--- Podfile
+++ Podfile
@@ -1,15 +1,23 @@
 source 'https://github.com/CocoaPods/Specs.git'
 source 'https://gitpub.rakuten-it.com/scm/eco/core-ios-specs.git'
 
-platform :ios, "8.0"
+install! 'cocoapods', :deterministic_uuids => false
+platform :ios, '8.0'
 use_frameworks!
 
-target 'Tests' do
-  pod 'RAnalytics', :path => './RAnalytics.podspec'
+abstract_target 'Common' do
   pod 'RAnalyticsBroadcast', :path => './RAnalyticsBroadcast.podspec'
   pod 'RDeviceIdentifier', :inhibit_warnings => true, :git => 'https://gitpub.rakuten-it.com/scm/eco/ios-deviceid.git'
   pod 'OCMock'
   pod 'OHHTTPStubs'
+
+  target 'Tests' do
+    pod 'RAnalytics', :path => './RAnalytics.podspec'
+  end
+
+  target 'CoreTests' do
+    pod 'RAnalytics/Core', :path => './RAnalytics.podspec'
+  end
 end
 
 # vim:syntax=ruby:et:sts=2:sw=2:ts=2:ff=unix:
---
+++ Tests/CoreSubspecTests.m
@@ -0,0 +1,39 @@
+@import XCTest;
+#import <RAnalytics/RAnalytics.h>
+#import "../RAnalytics/Core/Private/_SDKTracker.h"
+
+@interface RAnalyticsManager()<CLLocationManagerDelegate>
+@property(nonatomic, strong) NSMutableSet RSDKA_GENERIC(id<RAnalyticsTracker>) *trackers;
+- (instancetype)initSharedInstance;
+@end
+
+@interface CoreSubspecTests : XCTestCase
+@end
+
+@implementation CoreSubspecTests
+
+- (void)testRATTrackerIsNotAvailable
+{
+    XCTAssertNil(NSClassFromString(@"RAnalyticsRATTracker"));
+}
+
+- (void)testSDKTrackerIsAvailable
+{
+    XCTAssertNotNil(NSClassFromString(@"_SDKTracker"));
+}
+
+- (void)testSDKTrackerIsAddedToManager
+{
+    RAnalyticsManager *manager = [RAnalyticsManager.alloc initSharedInstance];
+    XCTAssert([manager.trackers containsObject:_SDKTracker.sharedInstance]);
+}
+
+- (void)testRATTrackerIsNotAddedToManager
+{
+    RAnalyticsManager *manager = [RAnalyticsManager.alloc initSharedInstance];
+    XCTAssert([manager.trackers containsObject:_SDKTracker.sharedInstance]);
+    XCTAssert(manager.trackers.count==1);
+}
+
+@end
+
