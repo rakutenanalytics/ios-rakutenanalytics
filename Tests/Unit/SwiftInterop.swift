2b06565dca1af954acf88579b86a68469a307004
Ta, Viet | Vito | GHRD <viet.ta@rakuten.com>
2018-07-18 10:11:05 +0900

    fix code review issues

--- Tests/Unit/SwiftInterop.swift
+++ Tests/Unit/SwiftInterop.swift
@@ -3,7 +3,6 @@ import RAnalytics
 import RDeviceIdentifier
 
 public class SwiftInterop : XCTestCase {
-    #if swift(>=3.0)
     @objc
     public class TrackerSwift : NSObject, Tracker {
         public var lastEvent : AnalyticsManager.Event?
@@ -38,7 +37,7 @@ public class SwiftInterop : XCTestCase {
             case AnalyticsManager.Event.Name.sessionEnd: break
             case AnalyticsManager.Event.Name.applicationUpdate: break
             case AnalyticsManager.Event.Name.login:
-                print("User with tracking id '\(state.userIdentifier)' just logged in!")
+                print("User with tracking id '\(state.userIdentifier ?? "")' just logged in!")
                 break
             case AnalyticsManager.Event.Name.logout:
                 if let logoutMethod = event.parameters[AnalyticsManager.Event.Parameter.logoutMethod] as? String {
@@ -57,18 +56,6 @@ public class SwiftInterop : XCTestCase {
             lastState = nil
             return false
         }
-    }
-
-    func assignMockDeviceIdentifier(_ deviceIdentifier : String?) {
-        // Ignore warning about unknown selector
-        AnalyticsManager.shared().perform(Selector("setDeviceIdentifier:"), with: deviceIdentifier)
-    }
-    public override func setUp() {
-        assignMockDeviceIdentifier("Test")
-    }
-
-    public override func tearDown() {
-        assignMockDeviceIdentifier(nil)
     }
 
     public func testSwiftInterop() {
@@ -103,108 +90,4 @@ public class SwiftInterop : XCTestCase {
         nc.post(name:NSNotification.Name.RAnalyticsUploadFailure, object:nil)
         nc.post(name:NSNotification.Name.RAnalyticsUploadSuccess, object:nil)
     }
-
-    #else
-    public class Tracker2 : NSObject, RAnalyticsTracker {
-    public var lastEvent : RAnalyticsEvent?
-    public var lastState : RAnalyticsState?
-    public func processEvent(event: RAnalyticsEvent, state: RAnalyticsState) -> Bool {
-    switch state.origin {
-    case RAnalyticsOrigin.InternalOrigin: break
-    case RAnalyticsOrigin.ExternalOrigin: break
-    case RAnalyticsOrigin.PushOrigin: break
-    case RAnalyticsOrigin.OtherOrigin: break
-    }
-
-    switch state.loginMethod {
-    case RAnalyticsLoginMethod.OneTapLoginLoginMethod:   break
-    case RAnalyticsLoginMethod.PasswordInputLoginMethod: break
-    case RAnalyticsLoginMethod.OtherLoginMethod:         break
-    }
-
-    if let _ = event.parameters["boo"] as? NSNumber {
-    print("just testing parameter unwrapping")
-    }
-
-    if state.isLoggedIn {
-    print("just checking the getter uses 'is'")
-    }
-
-    switch event.name {
-    case RAnalyticsInitialLaunchEventName:
-    lastEvent = (event.copy() as! RAnalyticsEvent)
-    lastState = (state.copy() as! RAnalyticsState)
-    return true
-    case RAnalyticsSessionStartEventName: break
-    case RAnalyticsSessionEndEventName: break
-    case RAnalyticsApplicationUpdateEventName: break
-    case RAnalyticsLoginEventName: break
-    case RAnalyticsLogoutEventName:
-    if let logoutMethod = event.parameters[RAnalyticsLogoutMethodEventParameter] as? String {
-    switch logoutMethod {
-    case RAnalyticsLocalLogoutMethod: break;
-    case RAnalyticsGlobalLogoutMethod: break;
-    default: break;
-    }
-    }
-    break
-    case RAnalyticsInstallEventName: break
-    default: break
-    }
-
-    lastEvent = nil
-    lastState = nil
-    return false
-    }
-    }
-
-    func assignMockDeviceIdentifier(deviceIdentifier : String?) {
-    // Ignore warning about unknown selector
-    RAnalyticsManager.sharedInstance().performSelector(Selector("setDeviceIdentifier:"), withObject:deviceIdentifier)
-    }
-    public override func setUp() {
-    assignMockDeviceIdentifier("Test")
-    }
-
-    public override func tearDown() {
-    assignMockDeviceIdentifier(nil)
-    }
-
-    public func testSwift2Interop() {
-    let rat : RAnalyticsRATTracker = RAnalyticsRATTracker.sharedInstance()
-    XCTAssertNotNil(rat)
-    rat.configureWithAccountId(477)
-    rat.configureWithApplicationId(999)
-
-    var event : RAnalyticsEvent = rat.eventWithEventType("foo", parameters: ["bar" : "baz"])
-    XCTAssertNotNil(event)
-
-    let manager = RAnalyticsManager.sharedInstance()
-    XCTAssertNotNil(manager)
-    XCTAssertFalse(manager.shouldUseStagingEnvironment)
-    XCTAssert(manager.shouldTrackLastKnownLocation)
-    XCTAssert(manager.shouldTrackAdvertisingIdentifier)
-    manager.shouldUseStagingEnvironment = false
-    manager.shouldTrackLastKnownLocation = true
-    manager.shouldTrackAdvertisingIdentifier = true
-
-    RAnalyticsManager.spoolRecord(RAnalyticsRecord(accountId: 477, serviceId: 999))
-
-    let tracker = Tracker2()
-    manager.addTracker(tracker)
-    manager.process(event)
-    XCTAssertNil(tracker.lastEvent)
-    XCTAssertNil(tracker.lastState)
-
-    event = RAnalyticsEvent(name: RAnalyticsInitialLaunchEventName, parameters: nil)
-    manager.process(event)
-    XCTAssertNotNil(tracker.lastEvent)
-    XCTAssertNotNil(tracker.lastState)
-
-    let nc = NSNotificationCenter.defaultCenter()
-    nc.postNotificationName(RAnalyticsWillUploadNotification, object:nil)
-    nc.postNotificationName(RAnalyticsUploadFailureNotification, object:nil)
-    nc.postNotificationName(RAnalyticsUploadSuccessNotification, object:nil)
-    }
-    #endif
 }
