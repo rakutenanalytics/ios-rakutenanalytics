0fb724beeecde6a58adb748902f64644745ce7c5
Malik Alayli <ts-alayli.malik@rakuten.com>
2020-09-15 11:08:28 +0200

    [iOS][Analytics] - [UIApplication _rat_statusBarOrientation] call fix

--- RAnalytics/Util/Private/_RStatusBarOrientationHandler.m
+++ RAnalytics/Util/Private/_RStatusBarOrientationHandler.m
@@ -11,22 +11,26 @@
 {
     self = [super init];
     if (self) {
-        if ([UIApplication _rat_respondsToSharedApplication]) {
-            _currentStatusBarOrientation = [UIApplication _rat_statusBarOrientation];
-            
-            // UIApplicationWillChangeStatusBarOrientationNotification is preferred to UIApplicationDidChangeStatusBarOrientationNotification in order to get the correct statusBarOrientation value before the payload is built with mori key where statusBarOrientation is set
-            [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationWillChangeStatusBarOrientationNotification object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    NSNumber *result = [note.userInfo objectForKey:UIApplicationStatusBarOrientationUserInfoKey];
-                    if (result) {
-                        self.currentStatusBarOrientation = [result intValue];
-                    }
-                });
-            }];
-            
-        } else { // [UIApplication sharedApplication] is not available for App Extension
-            _currentStatusBarOrientation = UIInterfaceOrientationPortrait; // default value
-        }
+        // As _RStatusBarOrientationHandler is instantiated from RAnalyticsRATTracker that is instantiated from [RAnalyticsManager load], [[UIApplication sharedApplication]] returns nil.
+        // Therefore we need to dispatch this operation in the main queue so [[UIApplication sharedApplication]] is not nil when this operation is executed.
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if ([UIApplication _rat_respondsToSharedApplication]) {
+                self.currentStatusBarOrientation = [UIApplication _rat_statusBarOrientation];
+
+            } else { // [UIApplication sharedApplication] is not available for App Extension
+                self.currentStatusBarOrientation = UIInterfaceOrientationPortrait; // default value
+            }
+        });
+        
+        // UIApplicationWillChangeStatusBarOrientationNotification is preferred to UIApplicationDidChangeStatusBarOrientationNotification in order to get the correct statusBarOrientation value before the payload is built with mori key where statusBarOrientation is set
+        [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationWillChangeStatusBarOrientationNotification object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                NSNumber *result = [note.userInfo objectForKey:UIApplicationStatusBarOrientationUserInfoKey];
+                if (result) {
+                    self.currentStatusBarOrientation = [result intValue];
+                }
+            });
+        }];
     }
     return self;
 }
