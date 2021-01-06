4fd9da83629f76b718e9e53edff8f5803d42a817
Michal Kalinowski <ts-michal.kalinowski@rakuten.com>
2021-01-07 00:12:36 +0100

    fix: fix toAfterTimeout Nimble clause

--- Tests/TestUtils.swift
+++ Tests/TestUtils.swift
@@ -2,22 +2,32 @@ import Nimble
 
 extension Expectation {
 
-    func toAfterTimeout(_ predicate: Predicate<T>,
+    func toAfterTimeout(file: FileString = #file,
+                        line: UInt = #line,
+                        _ predicate: Predicate<T>,
                         timeout: TimeInterval = 1.0) {
 
         let timeForExecution: TimeInterval = 1.0
         let totalTimeoutMS = Int((timeout + timeForExecution) * TimeInterval(USEC_PER_SEC))
         waitUntil(timeout: .microseconds(totalTimeoutMS)) { done in
-            DispatchQueue.global(qos: .userInteractive).async {
-                usleep(useconds_t(timeout * TimeInterval(USEC_PER_SEC)))
+            DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
+                expect(file: file, line: line, { try predicate.satisfies(self.expression).status }).to(equal(.matches))
+                done()
+            }
+        }
+    }
 
-                DispatchQueue.main.async {
-                    expect {
-                        try predicate.satisfies(self.expression)
-                    }.toNot(throwError())
+    func toAfterTimeoutNot(file: FileString = #file,
+                           line: UInt = #line,
+                           _ predicate: Predicate<T>,
+                           timeout: TimeInterval = 1.0) {
 
-                    done()
-                }
+        let timeForExecution: TimeInterval = 1.0
+        let totalTimeoutMS = Int((timeout + timeForExecution) * TimeInterval(USEC_PER_SEC))
+        waitUntil(timeout: .microseconds(totalTimeoutMS)) { done in
+            DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
+                expect(file: file, line: line, { try predicate.satisfies(self.expression).status }).to(equal(.doesNotMatch))
+                done()
             }
         }
     }
