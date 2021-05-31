2e89f96add4b29b783ccca3f6969180dca117b0e
Alayli, Malik <ts-malik.alayli@rakuten.com>
2021-06-01 03:08:01 +0900

    Pull request #394: Fix for generate-docs.sh
    Merge in ECO/core-ios-analytics from generate-docs-fix to master
    
    Squashed commit of the following:
    
    commit 11d676e4aa68d84a67fa1d639eb8282a8d3427a9
    Author: ts-malik.alayli <ts-malik.alayli@rakuten.com>
    Date:   Mon May 31 17:52:27 2021 +0200
    
        Fix for generate-docs.sh

--- generate-docs.sh
+++ generate-docs.sh
@@ -14,13 +14,13 @@ echo "📄 Installing Pods"
 bundle install && bundle exec pod install
 
 echo "📄 Generating Swift docs"
-SOURCEKITTEN doc --module-name RAnalytics -- clean build-for-testing -workspace CI.xcworkspace -scheme Tests -destination 'platform=iOS Simulator,name=iPhone 8' > swiftDoc.json
+$SOURCEKITTEN doc --module-name RAnalytics -- clean build-for-testing -workspace CI.xcworkspace -scheme Tests -destination 'platform=iOS Simulator,name=iPhone 8' > swiftDoc.json
 
 echo "📄 Generating Objective-C docs"
 mkdir ./docs-tmp
 find RAnalytics \( -name "*.h" -or -name "*.m" \) -exec cp {} ./docs-tmp \;
 mv ./docs-tmp ./RAnalytics/RAnalytics
-sourcekitten doc --objc ./RAnalytics/RAnalytics/RAnalytics.h -- -x objective-c  -isysroot $(xcrun --show-sdk-path --sdk iphonesimulator) -I ./RAnalytics -fmodules > objcDoc.json
+$SOURCEKITTEN doc --objc ./RAnalytics/RAnalytics/RAnalytics.h -- -x objective-c  -isysroot $(xcrun --show-sdk-path --sdk iphonesimulator) -I ./RAnalytics -fmodules > objcDoc.json
 
 echo "📄 Merging"
 jazzy --sourcekitten-sourcefile swiftDoc.json,objcDoc.json
