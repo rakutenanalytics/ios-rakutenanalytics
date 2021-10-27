2e9374daab61e6a32839d2c34ab0b67444b9490e
Alayli, Malik <ts-malik.alayli@rakuten.com>
2021-10-27 21:48:44 +0900

    Pull request #505: SDKCF-4463: Fix a Native iOS Crash - UUID.unconditionallyBridgeFromObjectiveC
    Merge in ECO/core-ios-analytics from feature/SDKCF-4463-UUID-unconditionallyBridgeFromObjectiveC-crash-fix to master
    
    Squashed commit of the following:
    
    commit 3ffb6e7ec9774ebc2f0df54fdb6bd23be1188627
    Author: ts-malik.alayli <ts-malik.alayli@rakuten.com>
    Date:   Tue Oct 26 19:13:46 2021 +0200
    
        SDKCF-4463: Fix a Native iOS Crash - UUID.unconditionallyBridgeFromObjectiveC

--- RAnalytics/Util/Private/ASIdentifierManager+Extensions.swift
+++ RAnalytics/Util/Private/ASIdentifierManager+Extensions.swift
@@ -5,7 +5,16 @@ protocol AdvertisementIdentifiable {
 }
 
 extension ASIdentifierManager: AdvertisementIdentifiable {
+    /// Fix a native iOS crash `UUID.unconditionallyBridgeFromObjectiveC `
+    /// https://bugs.swift.org/browse/SR-6143?focusedCommentId=33257&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel
+    private var safeAdvertisingIdentifier: UUID? {
+        perform(#selector(getter: Self.advertisingIdentifier))?.takeUnretainedValue() as? UUID
+    }
+
     var advertisingIdentifierUUIDString: String {
-        advertisingIdentifier.uuidString
+        guard let result = safeAdvertisingIdentifier?.uuidString else {
+            return ""
+        }
+        return result
     }
 }
