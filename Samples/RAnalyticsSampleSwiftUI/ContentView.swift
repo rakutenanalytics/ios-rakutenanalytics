e5af8cc973f9e303e65da319c929eb8b259bc7ef
Alayli, Malik <ts-malik.alayli@rakuten.com>
2022-02-22 16:07:52 +0900

    feat: Add and track a modal view in the SwiftUI sample app (SDKCF-4928)
    Merge in ECO/core-ios-analytics from feature/SDKCF-4928-swiftui-modal-view-tracking to master

--- Samples/RAnalyticsSampleSwiftUI/ContentView.swift
+++ Samples/RAnalyticsSampleSwiftUI/ContentView.swift
@@ -2,17 +2,26 @@ import SwiftUI
 import RAnalytics
 
 struct ContentView: View {
+    @State private var showingSheet = false
+    
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(alignment: .center, spacing: 40, content: {
                 NavigationLink(destination: LoginView()) {
                     Text("Login")
-                }.padding(40)
+                }
 
                 NavigationLink(destination: SettingsView()) {
                     Text("Settings")
                 }
-            }
+
+                Button("Sheet") {
+                    showingSheet.toggle()
+
+                }.sheet(isPresented: $showingSheet) {
+                    SheetView()
+                }
+            })
             .navigationTitle("RAnalyticsSampleSwiftUI")
             .navigationBarTitleDisplayMode(.inline)
             .rviewOnAppear(pageName: "Home")
