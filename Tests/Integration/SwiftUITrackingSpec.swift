9cac85a3dc2a8428c2de5c184b57b45639ae7fca
Alayli, Malik <ts-malik.alayli@rakuten.com>
2022-04-20 23:11:26 +0900

    fix: Fix the SwiftUI Tracking Test (SDKCF-5179)
    Merge in ECO/core-ios-analytics from fix/swiftui-tracking-test to master

--- Tests/Integration/SwiftUITrackingSpec.swift
+++ Tests/Integration/SwiftUITrackingSpec.swift
@@ -55,7 +55,7 @@ final class SwiftUITrackingSpec: QuickSpec {
 
                     try view.inspect().view(RView.self).callOnAppear()
 
-                    expect(taskIsCompleted).toAfterTimeout(beTrue(), timeout: 1.0)
+                    expect(taskIsCompleted).toAfterTimeout(beTrue(), timeout: 5.0)
                     expect(sessionMock.sentRequest).toEventuallyNot(beNil())
 
                     let ratPayload = sessionMock.sentRequest?.httpBody?.ratPayload
