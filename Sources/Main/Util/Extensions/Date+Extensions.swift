ab8e8ae263951acabc2a86594c7e6346d7168b01
ra-malik-alayli <90759890+ra-malik-alayli@users.noreply.github.com>
2023-05-02 10:31:28 +0200

    fix: Remove these useless parentheses (Sonarqube issue)

--- Sources/Main/Util/Extensions/Date+Extensions.swift
+++ Sources/Main/Util/Extensions/Date+Extensions.swift
@@ -5,7 +5,7 @@ extension Date {
     var timeInSeconds: UInt {
         let hoursInSeconds = ((Calendar.current.component(.hour, from: self) * 60) * 60)
         let minutesInSeconds = (Calendar.current.component(.minute, from: self) * 60)
-        return UInt((hoursInSeconds + minutesInSeconds))
+        return UInt(hoursInSeconds + minutesInSeconds)
     }
 
     static func timeIntervalBetween(current: Date, previous: Date) -> UInt {
