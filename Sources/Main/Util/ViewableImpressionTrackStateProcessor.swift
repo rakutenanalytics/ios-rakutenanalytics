89ebed15420ff479945545fdf2ac099abea79b8e
Dzmitry Kurski | ADED <dzmitry.a.kurski@rakuten.com>
2026-02-05 08:21:31 +0900

    improve: move some parts of code to separate files

---
+++ Sources/Main/Util/ViewableImpressionTrackStateProcessor.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+struct ViewableImpressionTrackStateProcessor {
+    static func process(item: ViewableImpressionTrackState,
+                        visibility: Double?,
+                        now: Date,
+                        minimumVisibility: Double,
+                        minimumDwell: TimeInterval) -> (qualified: ViewableImpressionQualifiedItemData?, remaining: TimeInterval?) {
+        guard let visibility = visibility else {
+            item.isVisible = false
+            item.firstVisibleTime = .distantPast
+            item.hasTriggered = false
+            item.visibilityPercentage = 0.0
+            return (nil, nil)
+        }
+
+        item.visibilityPercentage = visibility
+
+        let evaluation = ViewableImpressionEvaluation.evaluate(
+            visibility: visibility,
+            isVisible: item.isVisible,
+            firstVisibleTime: item.firstVisibleTime,
+            hasTriggered: item.hasTriggered,
+            minimumVisibility: minimumVisibility,
+            minimumDwell: minimumDwell,
+            now: now
+        )
+
+        item.isVisible = evaluation.isVisible
+        item.firstVisibleTime = evaluation.firstVisibleTime
+        item.hasTriggered = evaluation.hasTriggered
+
+        if evaluation.qualifies {
+            let dwellTime = now.timeIntervalSince(item.firstVisibleTime)
+            return (
+                ViewableImpressionQualifiedItemData(
+                    item: item.item,
+                    dwellTime: dwellTime,
+                    visibilityPercentage: visibility,
+                    itemPosition: item.itemPosition,
+                    screenName: item.screenName
+                ),
+                nil
+            )
+        }
+
+        return (nil, evaluation.remainingDwell)
+    }
+}
